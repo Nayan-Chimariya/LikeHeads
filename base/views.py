0be48cf3a8ba8e7341d33476501cd2480b72e134
Sleep deprived <cnayan789@gmail.com>
2024-07-11 19:02:19 +0545

    Removed option to choose host Host is the use that creats room

--- base/views.py
+++ base/views.py
@@ -103,14 +103,18 @@ def room(request,pk):
 
 @login_required(login_url='login')
 def createRoom(request):
-    form = RoomForm()
     if request.method == 'POST':
         form = RoomForm(request.POST)
-        if form.is_valid:
-            form.save()
-        return redirect('home')
-    context={'form':form}
-    return render(request,'base/room_form.html',context)
+        if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
+            return redirect('home')
+    else:
+        form = RoomForm()
+    
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
